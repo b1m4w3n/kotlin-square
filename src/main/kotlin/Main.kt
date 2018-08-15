f72d7dbd3f17c4b6357cba20bab0a821072aff56
b1m4w3n <b1m4w3n@gmail.com>
2018-08-15 10:15:41 -0400

    More fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
     println("result: " + max(2, 3))
     println("result2: " + max2(2, 3))
+    println("result3: " + max3(2, 3))
     println("Hello, world!")
 }
 
@@ -8,4 +9,7 @@ fun max(a: Int, b: Int): Int {
     return if (a > b) a else b
 }
 
-fun max2(a: Int, b: int): Int = if (a > b) a else b
+fun max2(a: Int, b: Int): Int = if (a > b) a else b
+
+// Should probably avoid using 'type inference' in function returns, in real world
+fun max3(a: Int, b: Int) = if (a > b) a else b
