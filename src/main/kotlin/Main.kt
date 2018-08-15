d69c5d6557f2ea876dbe8e2ab2fcb779bb6f1fe7
b1m4w3n <b1m4w3n@gmail.com>
2018-08-15 10:04:18 -0400

    Kotlin fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-    println("kotlin!")
+    println("result: " + max(2, 3))
+    println("result2: " + max2(2, 3))
+    println("Hello, world!")
 }
+
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+
+fun max2(a: Int, b: int): Int = if (a > b) a else b
