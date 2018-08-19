cfd90f92b4775fccd5eaf1c7014e825e7dd4d4bf
b1m4w3n <b1m4w3n@gmail.com>
2018-08-18 21:49:41 -0400

    More on string template.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,23 @@ fun main(args: Array<String>) {
     println("result2: " + max2(2, 3))
     println("result3: " + max3(2, 3))
     println("Hello, world!")
+
+    val message: String
+    if (true) {
+        message = "Success"
+    }
+    else {
+        message = "Failure"
+    }
+
+    val languages = arrayListOf("Java")
+    languages.add("Kotlin")
+    println("Languages: " + languages)
+
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello, $name!")
+
+    println("Hello, ${if (args.size > 0) args[0] else "someone"}")
 }
 
 fun max(a: Int, b: Int): Int {
