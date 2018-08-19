3c4d910904e4b8520e68cf93467351d5462c4a05
b1m4w3n <b1m4w3n@gmail.com>
2018-08-19 12:03:21 -0400

    More on class and property.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,6 +20,12 @@ fun main(args: Array<String>) {
     println("Hello, $name!")
 
     println("Hello, ${if (args.size > 0) args[0] else "someone"}")
+
+    val person = Person("Bob", true)
+    println("Hello, ${person.name}. Married: ${person.isMarried}")
+
+    val shape = Rectangle(5, 6)
+    println("Is this a square? ${shape.isSquare}")
 }
 
 fun max(a: Int, b: Int): Int {
@@ -30,3 +36,15 @@ fun max2(a: Int, b: Int): Int = if (a > b) a else b
 
 // Should probably avoid using 'type inference' in function returns, in real world
 fun max3(a: Int, b: Int) = if (a > b) a else b
+
+class Person(
+        val name: String,
+        var isMarried: Boolean
+)
+
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+    get() {
+        return height == width
+    }
+}
